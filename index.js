a6abc5683eabca901291cbbb69dbd78338be5040
Nikhil Singh <nikhilsinghmus@gmail.com>
2021-06-26 13:39:04 -0400

    Remove extra sample and redundant level settings, update object spacing.

--- index.js
+++ index.js
@@ -30,12 +30,12 @@ const session = new fluid.FluidSession({ bpm, dLibrary }, [
   { name: "chords2", gainDb: -14, plugins: [
     zebralette.cSupersaw(),
     tEqualizer.zero128()
-  ]}, // Intense chords
+  ] }, // Intense chords
   { name: "drums", gainDb: -2, plugins: [battery4.realestKit()], children: [ // Battery4 drums
     { name: "kick" },
-    { name: "snare", gainDb: -9.2 },
-    { name: "hat", gainDb: -20 },
-    { name: "fx", gainDb: -30 }
+    { name: "snare" },
+    { name: "hat" },
+    { name: "fx" }
   ]},
   { name: "bass", gainDb: -8, plugins: [zebralette.cBass()]}, // Simple bass
 
@@ -156,8 +156,8 @@ const fadeOutAllTracks = (duration=8) => {
  ***************/
 
 // Intro 1: @ unison
-session.useTechnique(tMute, {track: "chords1"})
-session.insertScore({...makeIntro(), drums: {tLibrary: {D: new techniques.MidiNote(77), F: new techniques.MidiNote(74)}, kick: "D", fx: "F"}})
+session.useTechnique(tMute, { track: "chords1" })
+session.insertScore({...makeIntro(), drums: { tLibrary: { D: new techniques.MidiNote(77), F: new techniques.MidiNote(74) }, kick: "D", fx: "F"}})
 arp6bTracks.forEach(trackName => session.useTechnique(tMute, { track: trackName }))
 
 // Intro 2: up a diatonic third
@@ -169,33 +169,33 @@ tracks.forEach((trackName) => session.useTechnique(tMute, { track: trackName }))
 session.insertScore(makeIntro(5))
 var tracks = ["arp6b3", "arp6b4"]
 tracks.forEach((trackName) => session.useTechnique(tMute, { track: trackName }))
-session.useTechnique(new fluid.techniques.TrackGainAutomation({ gainDb: -20}), {track: "arp6b"})
-session.useTechnique(new fluid.techniques.TrackGainAutomation({ gainDb: -20}), {track: "arp6b1"})
-session.useTechnique(new fluid.techniques.TrackGainAutomation({ gainDb: -20}), {track: "arp6b2"})
-session.useTechnique(new fluid.techniques.TrackGainAutomation({ gainDb: 0}), {track: "chords1"})
+session.useTechnique(new fluid.techniques.TrackGainAutomation({ gainDb: -20}), { track: "arp6b" })
+session.useTechnique(new fluid.techniques.TrackGainAutomation({ gainDb: -20}), { track: "arp6b1" })
+session.useTechnique(new fluid.techniques.TrackGainAutomation({ gainDb: -20}), { track: "arp6b2" })
+session.useTechnique(new fluid.techniques.TrackGainAutomation({ gainDb: 0}), { track: "chords1" })
 session.useTechnique(new fluid.techniques.TrackGainAutomationRamp({ gainDb: 6, curve: 0.8 }), { track: "chords1", startTime: session.editCursorTime + 0.01, duration: 3 })
 
 // Intro 4: octave up
-session.insertScore({...makeIntro(7), drums: {tLibrary: {S: new techniques.MidiNote(64), F: new techniques.MidiNote(78)}, snare: rampSnare, fx: "F"}})
-session.useTechnique(new fluid.techniques.TrackGainAutomation({ gainDb: -20}), {track: "arp6b3"})
-session.useTechnique(new fluid.techniques.TrackGainAutomation({ gainDb: -20}), {track: "arp6b4"})
-arp6bTracks.forEach(trackName => session.useTechnique(new fluid.techniques.TrackGainAutomation({ gainDb: -20}), { track: trackName }))
+session.insertScore({ ...makeIntro(7), drums: { tLibrary: { S: new techniques.MidiNote(64) }, snare: rampSnare } })
+session.useTechnique(new fluid.techniques.TrackGainAutomation({ gainDb: -20 }), { track: "arp6b3" })
+session.useTechnique(new fluid.techniques.TrackGainAutomation({ gainDb: -20 }), { track: "arp6b4" })
+arp6bTracks.forEach(trackName => session.useTechnique(new fluid.techniques.TrackGainAutomation({ gainDb: -20 }), { track: trackName }))
 arp6bTracks.forEach(trackName => session.useTechnique(tMute, { track: trackName }))
-arp6Tracks.forEach(trackName => session.useTechnique(new fluid.techniques.TrackGainAutomation({ gainDb: 6}), { track: trackName }))
+arp6Tracks.forEach(trackName => session.useTechnique(new fluid.techniques.TrackGainAutomation({ gainDb: 6 }), { track: trackName }))
 session.useTechnique(new fluid.techniques.TrackGainAutomationRamp({ gainDb: 0, curve: 0.8 }), { track: "chords1", startTime: session.editCursorTime + 0.01, duration: 0 })
 
 // Drop section
-session.insertScore({...drop, arp: undefined, drums: {...drop.drums, tLibrary: {...drop.drums.tLibrary, F: new techniques.MidiNote(74)}, fx: "F"}})
-session.insertScore({...drop, arp: undefined, drums: {...drop.drums, hat: altHat}})
-arp6bTracks.forEach(trackName => session.useTechnique(new fluid.techniques.TrackGainAutomation({ gainDb: -20}), { track: trackName }))
-arp6bTracks.forEach(trackName => session.useTechnique(new fluid.techniques.TrackGainAutomation({ gainDb: -6}), { track: trackName }))
-session.insertScore({...drop, drums: {...drop.drums, hat: altHat}})
-session.insertScore({...drop, drums: {...drop.drums, hat: altHat}, chords2: drop.chords2.slice(0, 15 * 16)})
+session.insertScore({ ...drop, arp: undefined, drums: { ...drop.drums, tLibrary: { ...drop.drums.tLibrary, F: new techniques.MidiNote(74) }, fx: "F" } })
+session.insertScore({ ...drop, arp: undefined, drums: { ...drop.drums, hat: altHat } })
+arp6bTracks.forEach(trackName => session.useTechnique(new fluid.techniques.TrackGainAutomation({ gainDb: -20 }), { track: trackName }))
+arp6bTracks.forEach(trackName => session.useTechnique(new fluid.techniques.TrackGainAutomation({ gainDb: -6 }), { track: trackName }))
+session.insertScore({ ...drop, drums: { ...drop.drums, hat: altHat }})
+session.insertScore({ ...drop, drums: { ...drop.drums, hat: altHat }, chords2: drop.chords2.slice(0, 15 * 16) })
 
 // Interlude section
 fadeOutAllTracks()
-session.insertScore({...drop, chords2: undefined, arp: undefined, arpb: undefined})
-session.insertScore({...drop, chords2: undefined, arp: undefined, arpb: undefined})
+session.insertScore({ ...drop, chords2: undefined, arp: undefined, arpb: undefined })
+session.insertScore({ ...drop, chords2: undefined, arp: undefined, arpb: undefined })
 
 
 /***************
